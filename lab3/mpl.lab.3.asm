4a125423f8a5b34babcfa857f0ee42dfaf27486d
solanoam <noam5456@gmail.com>
2018-11-06 22:44:17 +0200

    finish - no debug

--- lab3/mpl.lab.3.asm
+++ lab3/mpl.lab.3.asm
@@ -4,7 +4,7 @@
 ;
 ; Date          : 07/11/18
 ;
-; File          : mpl.lab3.part2.asm
+; File          : mpl.lab3.asm
 ;
 ; Hardware      : 8051 based processor
 ;
@@ -18,39 +18,25 @@ SHW     EQU   P3.3
 CSEG    AT    0000h
         JMP   MAIN
 
-CSEG    AT    0003h     ; address of the interrupt
-        CPL   P3.4      ; toggles the LED
-        SETB  FLAG      ; flag bit
-        CLR   EX0        ; disabling interrupts
+CSEG    AT    0013H     ; address of the external intrupt
+        PUSH  ACC
+        PUSH  PSW
+        MOV   A, TH1
+        ADD   A, #05h
+        MOV   TH1, A
+        POP   PSW
+        POP   ACC
         RETI
+
+CSEG    AT    001Bh     ; address of the timer 1 interrupt
+        CPL   P3.4      ; toggles the LED
+        RETI
+
 
 CSEG    AT    0100h
 MAIN:
-		CLR   FLAG
-		SETB  IT0       ; setting interrupt to edge trigged
-		SETB  P3.2      ; Pulling the switch up
-		SETB  EX0       ; Enable exact interrupt 0
-		SETB  EA        ; Enable interrupts
-LOOP:
-		JB    FLAG , LOOPBEG ;checking flag bit
-		JMP   LOOP
-LOOPBEG:
-        CLR		FLAG			; clear flag
-        CALL 	DELAY			; call 200ms delay
-        CLR		IE0
-        SETB	EX0
-        JMP		LOOP	    ; loop
-DELAY:
-        MOV     R7,#16          ; 16 * 12.41ms = 198.56ms
-        MOV     R6,#200         ; 200 * 62.1us = 12.42ms
-        MOV     R5,#229         ; 229 * 0.271us = 62.1us
-DLY:    DJNZ    R5,$            ;
-		    MOV		  R5, #229        ; repeat 229 times (62.1us)
-        DJNZ    R6,DLY          ;
-        MOV		  R6, #200        ; repeat 200 times (12.4ms delay)
-		    DJNZ    R7,DLY          ; repeat 160 times (200ms delay)
-        RET
-BSEG
-FLAG: 	DBIT 1
-
+        MOV   TMOD, #00100000b
+        SETB  EX
+        SETB  IT1
+        SETB  EX1
 END
