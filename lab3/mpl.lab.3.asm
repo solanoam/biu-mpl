6d8c98795d4f72e71d689cb151f7789973712252
solanoam <31572763+solanoam@users.noreply.github.com>
2018-11-07 11:46:42 +0200

    finished

--- lab3/mpl.lab.3.asm
+++ lab3/mpl.lab.3.asm
@@ -13,20 +13,14 @@
 ;
 ;********************************************************************
 LED     EQU   P3.4      ; P3.4 is red LED on eval board
-SHW     EQU   P3.3
+SWT     EQU   P3.3		; Switch
 
 CSEG    AT    0000h
         JMP   MAIN
 
-CSEG    AT    0013H     ; address of the external intrupt
-        PUSH  ACC
-        PUSH  PSW
-        MOV   A, TH1
-        ADD   A, #05h
-        MOV   TH1, A
-        POP   PSW
-        POP   ACC
-        RETI
+CSEG    AT    0013H     ; address of the external intrupt 1
+        SETB  FLAG
+		RETI
 
 CSEG    AT    001Bh     ; address of the timer 1 interrupt
         CPL   P3.4      ; toggles the LED
@@ -35,8 +29,29 @@ CSEG    AT    001Bh     ; address of the timer 1 interrupt
 
 CSEG    AT    0100h
 MAIN:
-        MOV   TMOD, #00100000b
-        SETB  EX
-        SETB  IT1
+        ORL   TMOD, #11110000b
+		ANL	  TMOD, #00101111b
+        CLR   TF1
+		SETB  TR1
+		MOV   TH1, #00h
+		SETB  IT1
         SETB  EX1
+		SETB  ET1
+		SETB  EA
+LOOP:
+		JB    FLAG , EI1CONT 
+		JMP   LOOP
+EI1CONT:
+		PUSH  ACC		
+        PUSH  PSW
+		CLR   FLAG
+        MOV   A, TH1
+        ADD   A, #05h
+        MOV   TH1, A
+        POP   PSW
+        POP   ACC
+        JMP   LOOP
+
+BSEG
+FLAG: 	DBIT 1
 END
